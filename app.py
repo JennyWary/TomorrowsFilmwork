cefe5c97565f066690e9bcdfc6631470bcc9a19e
Falyrion <29983908+Falyrion@users.noreply.github.com>
2025-03-11 18:41:54 +0100

    Update

---
+++ app.py
@@ -0,0 +1,57 @@
+import os
+from flask import Flask, render_template, request
+
+
+# Tell flask to turn this file into a flask-app
+app = Flask(__name__)
+
+# Define a function that gets called if the website domain equals main page
+# This function returns a html file and renders it to the screen
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/corvette")
+def corvette():
+    return render_template("corvette.html")
+
+
+@app.route("/klettern-im-elbsandsteingebirge")
+def climbing():
+    return render_template("Klettern_im_Elbsandsteingebirge.html")
+
+
+@app.route("/daf")
+def daf():
+    return render_template("daf.html")
+
+
+@app.route("/iceland")
+def iceland():
+    return render_template("iceland.html")
+
+
+@app.route("/ccl-label")
+def cll():
+    return render_template("cll.html")
+
+
+@app.route("/falten-des-kranichs")
+def falten_des_kranichs():
+    return render_template("kranichs.html")
+
+
+@app.route("/impressum")
+def impressum():
+    return render_template("impressum.html")
+
+
+@app.route("/datenschutz")
+def datenschutz():
+    return render_template("datenschutz.html")
+
+
+# Run app
+if __name__ == '__main__':
+    app.run()
